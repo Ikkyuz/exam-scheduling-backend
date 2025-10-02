1995f292b6937beba0248432b72850636d6364f9
Ikkyuz <djdjv999@gmail.com>
2025-10-02 15:26:55 +0700

    แก้ไข validation error “Expected object” สำหรับ enrollments

--- src/features/enrollment/enrollment.schema.ts
+++ src/features/enrollment/enrollment.schema.ts
@@ -17,21 +17,22 @@ export const EnrollmentCreateUpdateSchema = t.Omit(EnrollmentSchema, [
 export const EnrollmentWithRelationsSchema = t.Composite([
     EnrollmentSchema,
     t.Object({
-        class: t.Array(t.Object({
+        class: t.Object({
             id: t.String(),
             name: t.String(),
             level: t.UnionEnum(["Pvoc", "Pvs"]),
             classYear: t.String(),
-        })),
-        course: t.Array(t.Object({
+        }),
+        course: t.Object({
             id: t.String(),
             code: t.String(),
             name: t.String(),
             duration: t.Number(),
             examType: t.UnionEnum(["InSchedule", "OutSchedule"]),
-        })),
+        }),
     }),
 ]);
+
 
 export type Enrollment = typeof EnrollmentSchema.Type;
 export type EnrollmentCreateUpdate = typeof EnrollmentCreateUpdateSchema.Type;
